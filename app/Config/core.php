9045abac4b031a0c55994287c7eb07ffadf44a46
hassy <hassy1983@gmail.com>
2013-06-15 17:01:48 +0900

    modify_core.php

--- app/Config/core.php
+++ app/Config/core.php
@@ -194,12 +194,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVaijfeiajwiejfivniR2G0FgaC9mi');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '76858478957829342496749683645');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
