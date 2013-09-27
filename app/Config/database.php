38c0562d041f592890940857f0b4c1cee871e1ca
hassy <hassy1983@gmail.com>
2013-09-27 09:19:11 +0900

    modify heroku database settings

--- app/Config/database.php
+++ app/Config/database.php
@@ -54,14 +54,33 @@
  * For MySQL to connect via socket specify the `unix_socket` parameter instead of `host` and `port`
  */
 class DATABASE_CONFIG {
+
     function __construct() {
+
         $url = parse_url(getenv('CLEARDB_DATABASE_URL'));
+
         $this->default = array(
+            'datasource' => 'Database/Mysql',
+            'persistent' => false,
             'host' => $url['host'],
             'login' => $url['user'],
             'password' => $url['pass'],
             'database' => substr($url['path'],1),
+            'prefix' => '',
+            //'encoding' => 'utf8',
         );
-        $this->test = array();
+
+        $this->test = array(
+            'datasource' => 'Database/Mysql',
+            'persistent' => false,
+            'host' => 'localhost',
+            'login' => 'user',
+            'password' => 'password',
+            'database' => 'test_database_name',
+            'prefix' => '',
+            //'encoding' => 'utf8',
+        );
+
     }
+
 }
