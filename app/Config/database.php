fade8ee88bc46497926c4773b413348c6bbbd800
hassy <hassy1983@gmail.com>
2013-10-26 22:21:51 +0900

    modify_database.php

--- app/Config/database.php
+++ app/Config/database.php
@@ -55,32 +55,25 @@
  */
 class DATABASE_CONFIG {
 
-    function __construct() {
+	public $default = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'peraichi',
+		'password' => 'flyer',
+		'database' => 'peraichi',
+		'prefix' => '',
+		//'encoding' => 'utf8',
+	);
 
-        $url = parse_url(getenv('CLEARDB_DATABASE_URL'));
-
-        $this->default = array(
-            'datasource' => 'Database/Mysql',
-            'persistent' => false,
-            'host' => $url['host'],
-            'login' => $url['user'],
-            'password' => $url['pass'],
-            'database' => substr($url['path'],1),
-            'prefix' => '',
-            //'encoding' => 'utf8',
-        );
-
-        $this->test = array(
-            'datasource' => 'Database/Mysql',
-            'persistent' => false,
-            'host' => 'localhost',
-            'login' => 'user',
-            'password' => 'password',
-            'database' => 'test_database_name',
-            'prefix' => '',
-            //'encoding' => 'utf8',
-        );
-
-    }
-
+	public $test = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'user',
+		'password' => 'password',
+		'database' => 'test_database_name',
+		'prefix' => '',
+		//'encoding' => 'utf8',
+	);
 }
